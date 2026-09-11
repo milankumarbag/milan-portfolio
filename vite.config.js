5cef8d637373d04e1f387db0a27236f0c3e4cd28
milankumarbag <165384975+milankumarbag@users.noreply.github.com>
2026-09-11 20:36:16 +0530

    Configure GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/milan-portfolio/',
   plugins: [react()],
   server: {
     port: 3000,
